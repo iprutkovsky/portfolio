dba4a1584969622401739b4ebe9d720cf971b63a
Ilia Prutkovsky <43461476+iprutkovsky@users.noreply.github.com>
2021-03-24 22:56:44 -0400

    Update animation.effects.js

--- js/animation.effects.js
+++ js/animation.effects.js
@@ -22,37 +22,37 @@
 	}
 
 	// skills screen saver
-	let screenSaver = () =>
-		$(document).ready(function () {
-			let s = 1000;
-			let arr = ['Java', 'JDBC', 'Servlets', 'Spring', 'SQL', 'MYSQL', 'OracleSQL',
-				'Angular 8', 'Angular 9', 'Angular 10', 'TypeScript', 'Angular JS', 'HTML5', 'CSS3',
-				'Javascript', 'RxJS', 'JQuery', 'Node.js', 'Jenkins', 'GitHub',
-				'GitLab', 'AWS', 'S3', 'GC', 'Wordpress'];
-			setTimeout(function () {
-				let container = document.querySelector('.container');
-				container.innerHTML = '';
-				arr.map(v => {
-					let blocks = document.createElement('div');
-					$(blocks).addClass('blk');
-					blocks.innerHTML = v;
-					container.appendChild(blocks);
-				})
+	// let screenSaver = () =>
+	// 	$(document).ready(function () {
+	// 		let s = 1000;
+	// 		let arr = ['Java', 'JDBC', 'Servlets', 'Spring', 'SQL', 'MYSQL', 'OracleSQL',
+	// 			'Angular 8', 'Angular 9', 'Angular 10', 'TypeScript', 'Angular JS', 'HTML5', 'CSS3',
+	// 			'Javascript', 'RxJS', 'JQuery', 'Node.js', 'Jenkins', 'GitHub',
+	// 			'GitLab', 'AWS', 'S3', 'GC', 'Wordpress'];
+	// 		setTimeout(function () {
+	// 			let container = document.querySelector('.container');
+	// 			container.innerHTML = '';
+	// 			arr.map(v => {
+	// 				let blocks = document.createElement('div');
+	// 				$(blocks).addClass('blk');
+	// 				blocks.innerHTML = v;
+	// 				container.appendChild(blocks);
+	// 			})
 
-				let animeShuffle = () =>
-					anime({
-						targets: '.blk',
-						translateX: () => anime.random(-550, 550),
-						translateY: () => anime.random(-100, 100),
-						scale: () => anime.random(.5, 3),
-						easing: 'linear',
-						duration: 9 * s,
-						delay: anime.stagger(9),
-						complete: animeShuffle
-					});
-				animeShuffle();
-			}, s);
-		});
+	// 			let animeShuffle = () =>
+	// 				anime({
+	// 					targets: '.blk',
+	// 					translateX: () => anime.random(-550, 550),
+	// 					translateY: () => anime.random(-100, 100),
+	// 					scale: () => anime.random(.5, 3),
+	// 					easing: 'linear',
+	// 					duration: 9 * s,
+	// 					delay: anime.stagger(9),
+	// 					complete: animeShuffle
+	// 				});
+	// 			animeShuffle();
+	// 		}, s);
+	// 	});
 
 	// Containers fading script
 	let contentWayPoint = function () {
